c4645f5a864c5456ac45d68bec0eed1161544822
Akanksha <a31695132@gmail.com>
2026-07-06 08:51:02 +0530

    updated some files

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  plugins: [react(), tailwindcss()],
+  build: {
+    // This stops small warning fragments from triggering a hard exit code 2 crash
+    chunkSizeWarningLimit: 1600,
+  }
 })
